6e9e963fced64dcae39a5dc08bd801e38d15c6da
Showmick Kar <showmick12@gmail.com>
2021-07-05 18:48:44 +0600

    Update and rename LinearAndCircularArrays.java to Array.java

--- LAB01/Array.java
+++ LAB01/Array.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
-public class Main {
+public class Array {
     public static void main(String[] args) {
         /* Linear Arrays */
         // 1
