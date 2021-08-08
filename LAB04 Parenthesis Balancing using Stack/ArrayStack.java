834cb3f34090e2d39fd3ffd39eb28fa754daf420
Showmick Kar <showmick12@gmail.com>
2021-08-08 15:36:12 +0600

    Update ArrayStack.java

--- LAB04 Parenthesis Balancing using Stack/ArrayStack.java
+++ LAB04 Parenthesis Balancing using Stack/ArrayStack.java
@@ -6,6 +6,11 @@ public class ArrayStack<T> implements Stack<T> {
     private int size = 0;
     private int pointer = 0;
 
+    /**
+     * pushes a new element to the stack
+     *
+     * @param element
+     */
     @Override
     public void push(T element) {
         try {
@@ -19,6 +24,9 @@ public class ArrayStack<T> implements Stack<T> {
         }
     }
 
+    /**
+     * pops the last element from the stack
+     */
     @Override
     public void pop() {
         try {
@@ -26,16 +34,22 @@ public class ArrayStack<T> implements Stack<T> {
                 throw new EmptyStackException();
             }
             pointer--;
-        } catch(EmptyStackException e) {
+        } catch (EmptyStackException e) {
             System.out.println(e);
         }
     }
 
+    /**
+     * @return the value of the last element of the stacks
+     */
     @Override
     public T peek() {
         return (pointer > 0) ? innerArray[pointer - 1] : null;
     }
 
+    /**
+     * @return true if the stack is empty
+     */
     @Override
     public boolean isEmpty() {
         return pointer <= 0;
