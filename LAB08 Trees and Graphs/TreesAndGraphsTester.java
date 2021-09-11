5183204f13cef857af10d61aa8dce30e9410c11b
Showmick Kar <showmick12@gmail.com>
2021-09-12 00:58:46 +0600

    Reverted previous commit

--- LAB08 Trees and Graphs/TreesAndGraphsTester.java
+++ LAB08 Trees and Graphs/TreesAndGraphsTester.java
@@ -1,116 +1,4 @@
-# LAB08 Trees and Graphs Solutions
-
-## Definition of a Tree Node
-``` java
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        public TreeNode(int n) {
-            val = n;
-            left = null;
-            right = null;
-        }
-    }
-```
-
-## Assuming this is out input tree
-```
-               1 
-              / \
-             2   3
-            / \   \
-           4   8   10
-         /
-        5
-```
-
-## Problem 01: RECURSIVELY calculate the height of a tree.
-``` java
-    private int dfs(TreeNode root, int height) {
-        if (root == null) return height;
-        return Math.max(dfs(root.left, height), dfs(root.right, height + 1));
-    }
-
-    public int heightOfTree(TreeNode root) {
-        return dfs(root, 0);
-    }
-```
-## Problem 02: RECURSIVELY calculate the level of a Node in a tree.
-``` java
-    private int levelOfNodeHelper(TreeNode root, TreeNode target, int level) {
-        if (root == null) return 0;
-        if (root.val == target.val) return level;
-        int leftSubtree = levelOfNodeHelper(root.left, target, level);
-        if (leftSubtree > 0) return leftSubtree;
-        int rightSubtree = levelOfNodeHelper(root.right, target, level);
-        if (rightSubtree > 0) return rightSubtree;
-        return 0;
-    }
-
-    public int levelOfNode(TreeNode root, TreeNode target) {
-        // will return 0 if the target node is not present int the binary tree
-        return levelOfNodeHelper(root, target, 1);
-    }
-```
-
-## Problem 03: Print elements of all the Nodes of a tree using Pre-order Traversal.
-``` java
-    public void preOrderTraversal(TreeNode root) {
-        if (root == null) return;
-        System.out.print(root.val + " ");
-        preOrderTraversal(root.left);
-        preOrderTraversal(root.right);
-    }
-```
-
-## Problem 04: Print elements of all the Nodes of a tree using In-order Traversal.
-``` java
-    public void inOrderTraversal(TreeNode root) {
-        if (root == null) return;
-        inOrderTraversal(root.left);
-        System.out.print(root.val + " ");
-        inOrderTraversal(root.right);
-    }
-```
-
-## Problem 05: Print elements of all the Nodes of a tree using Post-order Traversal.
-``` java
-    public void postOrderTraversal(TreeNode root) {
-        if (root == null) return;
-        postOrderTraversal(root.left);
-        postOrderTraversal(root.right);
-        System.out.print(root.val + " ");
-    }
-```
-
-## Problem 06: Write a method which will evaluate whether two trees are exactly same or not.
-``` java
-    public boolean sameTree(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) return true;
-        if (root1 == null || root2 == null) return false;
-        return root1.val == root2.val && sameTree(root1.left, root2.left) && sameTree(root1.right, root2.right);
-    }
-```
-
-## Problem 07: Write a method which will return a copy (new tree) of a given tree.
-``` java
-    public TreeNode copyTree(TreeNode root) {
-        if (root == null) return null;
-        TreeNode newRoot = new TreeNode(root.val);
-        newRoot.left = copyTree(root.left);
-        newRoot.right = copyTree(root.right);
-        return newRoot;
-    }
-```
-
-## Problem 08: An adjacency matrix is given below. Draw the equivalent graph.
-<img src="graph.jpeg" width="500">
-
-## For testing purpose all the code including the tester class has been attached below  
-
-``` java
-    public class TreesAndGraphsTester {
+public class TreesAndGraphsTester {
     public static void main(String[] args) {
         TreesAndGraphsTester tester = new TreesAndGraphsTester();
         displayTree();
@@ -272,10 +160,10 @@ class TreeNode {
         right = null;
     }
 }
-```
 
-## Here is the output
-```
+/*
+Here is the output
+
 ################################
 Tree Display
 ################################
@@ -283,9 +171,9 @@ Tree Display
       / \
      2   3
     / \   \
-  4   8   10
- /
-5
+   4   8   10
+  /
+ 5
 
 ################################
 Problem 01: RECURSIVELY calculate the height of a tree
@@ -323,4 +211,5 @@ Problem 07: Write a method which will return a copy (new tree) of a given tree
 ################################
 Initial Tree: 1 2 4 5 8 3 10 
 Copied new Tree: 1 2 4 5 8 3 10 
-```
+
+*/
